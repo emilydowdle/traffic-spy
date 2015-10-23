3351f57087d55708ae8ba51b83cdee065ca3b1d2
Emily Dowdle <emily@editingemily.com>
2015-10-22 19:22:41 -0600

    rake test setup

--- test/controllers/payload_test.rb
+++ test/controllers/payload_test.rb
@@ -13,7 +13,7 @@ class PayloadTest < Minitest::Test
                             \"eventName\": \"socialLogin\",
                             \"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",
                             \"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",
-                            \"ip\":\"63.29.38.211\" }"
+                            \"ip\":\"63.29.38.211\" }" }
 
   def app
     TrafficSpy::Server
