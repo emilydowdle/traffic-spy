51446950b3600c1cb15189b35976b8128b5397a8
Emily Dowdle <emily@editingemily.com>
2015-10-23 16:03:13 -0600

    browser parsing

--- app/models/identifier_dashboard.rb
+++ app/models/identifier_dashboard.rb
@@ -2,53 +2,53 @@ require_relative '../controllers/server'
 
 class Dashboard
 
-  def self.populate_dashboard(identifier)
-    url_data = sort_urls_by_visit(identifier)
-    display_browser_data(identifier, url_data)
+  def self.sort_urls_by_visit(identifier)
+    source = Source.find_by(identifier: identifier)
+    source.payloads.group("url").count.sort_by {|k, v| v}.reverse
   end
 
-  def sort_urls_by_visit(identifier, url_data={})
-    url_data = {}
+  def self.browser_data(identifier, browser_hsh={})
     source = Source.find_by(identifier: identifier)
-    source.payloads.each do |payload|
-      if url_data.has_key?(payload.url)
-        url_data[payload.url][:visited] +=1
-      else
-        url_data[payload.url] = { visited: 1 }
-      end
-      url_data.sort_by {|k, v| v}
+
+    raw_user_agent = source.payloads.pluck("userAgent").count
+    binding.pry
+    raw_user_agent.map do |data|
+      data = UserAgent.parse(data).browser
     end
-    url_data
+    raw_user_agent
+    # source.payloads.group("userAgent".user_agent.browser).count.sort_by {|k, v| v}.reverse
   end
 
-  def display_browser_data(identifier, url_data)
-    source = Source.find_by(identifier: identifier)
-    source.payloads.each do |payload|
-      binding.pry
-      browser = UserAgent.parse(payload.userAgent).browser
-      if browser_data.has_key?(browser)
-        browser_data[browser] += 1
-      else
-        browser_data[browser] = 1
-      end
-      browser_data.sort_by {|k, v| v}
-    end
-    browser_data.to_a
+  # def self.sort_urls_by_visit(identifier)
+  #   source = Source.find_by(identifier: identifier)
+  #   source.payloads.group("url").count.sort_by {|k, v| v}.reverse
+  # end
+  #
+  # def self.sort_urls_by_visit(identifier)
+  #   source = Source.find_by(identifier: identifier)
+  #   arr = source.payloads.group("url").count.sort_by {|k, v| v}.reverse
+  # end
+
+  # def sort_urls_by_visit(identifier, url_data={})
+  #   url_data = {}
+  #   source = Source.find_by(identifier: identifier)
+  #   source.payloads.each do |payload|
+  #     if url_data.has_key?(payload.url)
+  #       url_data[payload.url][:visited] +=1
+  #     else
+  #       url_data[payload.url] = { visited: 1 }
+  #     end
+  #     url_data.sort_by {|k, v| v}
+  #   end
+  #   url_data
+  # end
+
+  def display_browser_data(identifier)
+
   end
 
   def display_os_data(identifier)
-    os_data = {}
-    source = Source.find_by(identifier: identifier)
-    source.payloads.each do |payload|
-      os = UserAgent.parse(payload.userAgent).platform
-      if os_data.has_key?(os)
-        os_data[os] += 1
-      else
-        os_data[os] = 1
-      end
-      os_data.sort_by {|k, v| v}
-    end
-    os_data.to_a
+
   end
 
 end
--- test/models/identifier_dashboard_test.rb
+++ test/models/identifier_dashboard_test.rb
@@ -3,22 +3,20 @@ require_relative "../../app/controllers/server"
 
 class DashboardTest < Minitest::Test
 
-  # def test_urls_sorted_by_visit
-  #   create_source_and_payload
-  #   create_second_source_and_payload
-  #   create_third_source_and_payload
-  #
-  #   assert_equal ({"http://jumpstartlab.com/blog"=>{:visited=>1}}), Dashboard.new.sort_urls_by_visit("jumpstartlab")
-  #   assert_equal ({"http://google.com/analytics"=>{:visited=>1}}), Dashboard.new.sort_urls_by_visit("google")
-  # end
-
-  def test_browser_data_across_all_requests
+  def test_urls_sorted_by_visit
     create_source_and_payload
     create_second_source_and_payload
     create_third_source_and_payload
 
-    assert_equal [["Chrome", 1]], Dashboard.populate_dashboard("jumpstartlab")
-    assert_equal [["Chrome", 1]], Dashboard.populate_dashboard("google")
+    assert_equal [["http://jumpstartlab.com/blog", 1]], Dashboard.sort_urls_by_visit("jumpstartlab")
+    assert_equal [["http://google.com/analytics", 1]], Dashboard.sort_urls_by_visit("google")
+  end
+
+  def test_browser_data_across_all_requests
+    create_additional_payload
+
+    assert_equal [["Chrome", 1]], Dashboard.browser_data("jumpstartlab")
+    assert_equal [["Chrome", 1]], Dashboard.browser_data("google")
   end
   #
   # def test_os_data_across_all_requests
