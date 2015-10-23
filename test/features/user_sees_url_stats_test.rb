9bf1af2cb3efafdfce0c793132eb6765cae6068d
Marlo Major <MarloMajor@yahoo.com>
2015-10-23 02:27:25 -0600

    url stats:url does not exist in progress

--- test/features/user_sees_url_stats_test.rb
+++ test/features/user_sees_url_stats_test.rb
@@ -2,6 +2,24 @@ require_relative '../test_helper'
 
 class UrlStatsTest < Minitest::Test
 
+  def create_source_and_payload_for_nil_url
+    source = Source.create({ identifier: "jumpstartlab",
+                             rootUrl:    "http://jumpstartlab.com" })
+
+    source.payloads.create({ "url"=>"",
+                             "requestedAt"=>"2013-02-16 21:38:28 -0700",
+                             "respondedIn"=>37,
+                             "referredBy"=>"http://jumpstartlab.com",
+                             "requestType"=>"GET",
+                             "parameters"=>[],
+                             "eventName"=>"socialLogin",
+                             "userAgent"=>
+                             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                             "resolutionWidth"=>"1920",
+                             "resolutionHeight"=>"1280",
+                             "ip"=>"63.29.38.211" })
+  end
+
   def test_user_can_see_page
     create_source_and_payload
     visit '/sources/jumpstartlab/urls/blog'
@@ -19,12 +37,11 @@ class UrlStatsTest < Minitest::Test
     assert_equal 3, create_source_and_payload[:id]
   end
 
-  def test_user_can_see_agents
-    
+  def test_user_gets_message_url_not_requested
     visit '/sources/jumpstartlab/urls/blog'
 
     assert_equal '/sources/jumpstartlab/urls/blog', current_path
-
+    assert "URL has not been requested", create_source_and_payload_for_nil_url[:url].nil? || create_source_and_payload_for_nil_url[:url].empty?
+    binding.pry
   end
 end
-    # binding.pry
