fdbec315f13606b1c8f476533a72a8ed3a0d696f
ZooBot Mars <sasa345@yahoo.com>
2015-10-25 14:55:01 -0600

    progress on event feature tests

--- test/features/user_sees_aggregate_event_data_test.rb
+++ test/features/user_sees_aggregate_event_data_test.rb
@@ -2,6 +2,50 @@ require_relative "../test_helper"
 
 class EventTest < FeatureTest
 
+  def create_event_test_payloads
+    source = Source.create({ identifier: "jumpstartlab",
+                             rootUrl:    "http://jumpstartlab.com" })
+
+    source.payloads.create({ "url"=>"http://jumpstartlab.com/blog",
+                             "requestedAt"=>"2013-02-16 21:38:28 -0700",
+                             "respondedIn"=>37,
+                             "referredBy"=>"http://jumpstartlab.com",
+                             "requestType"=>"GET",
+                             "eventName"=>"socialLogin",
+                             "userAgent"=>
+                             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                             "resolutionWidth"=>"1920",
+                             "resolutionHeight"=>"1280",
+                             "ip"=>"63.29.38.211" })
+
+    source.payloads.create({ "url"=>"http://jumpstartlab.com/blog",
+                             "requestedAt"=>"2014-02-16 10:38:28 -0700",
+                             "respondedIn"=>37,
+                             "referredBy"=>"http://jumpstartlab.com",
+                             "requestType"=>"GET",
+                             "parameters"=>[],
+                             "eventName"=>"socialLogin",
+                             "userAgent"=>
+                             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                             "resolutionWidth"=>"1920",
+                             "resolutionHeight"=>"1280",
+                             "ip"=>"63.29.38.211" })
+
+     source.payloads.create({ "url"=>"http://jumpstartlab.com/blog",
+                              "requestedAt"=>"2014-02-16 19:38:28 -0700",
+                              "respondedIn"=>37,
+                              "referredBy"=>"http://jumpstartlab.com",
+                              "requestType"=>"GET",
+                              "parameters"=>[],
+                              "eventName"=>"bannerClick",
+                              "userAgent"=>
+                              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                              "resolutionWidth"=>"1920",
+                              "resolutionHeight"=>"1280",
+                              "ip"=>"63.29.38.211" })
+
+  end
+
   def test_users_view_is_current_path
     create_source_and_payload
     visit '/sources/jumpstartlab/events'
@@ -15,6 +59,13 @@ class EventTest < FeatureTest
     create_source_and_payload
     visit '/sources/jumpstartlab/events'
     assert page.has_text? ("Events")
+  end
+
+  def test_user_sees_most_received_event_to_least_received_event
+    create_event_test_payloads
+    binding.pry
+    visit '/sources/jumpstartlab/events'
+    assert page.has_content? ("Most received event to least received event")
   end
 
 end
