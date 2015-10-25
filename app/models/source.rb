5e087897b8d71f758311c3a2aea98dd6f7a772b9
ZooBot Mars <sasa345@yahoo.com>
2015-10-25 11:55:07 -0600

    event tests working

--- app/models/source.rb
+++ app/models/source.rb
@@ -3,10 +3,7 @@ class Source < ActiveRecord::Base
 
   has_many :payloads
   # has_many :events, through: :payloads
-  attr_accessor :time_match_hash
 
-  @time_match_hash = {1=> "12am - 1am",
-                      2=> "1am - 2am"}
 
   def sort_events_received
     event_hash = Hash.new(0)
@@ -17,6 +14,29 @@ class Source < ActiveRecord::Base
   end
 
   def find_event_data_over_24hrs
+    time_match_hash = {1=> "midnight - 1am",
+      2=> "1am - 2am",
+      3=> "2am - 3am",
+      4=> "3am - 4am",
+      5=> "4am - 5am",
+      6=> "5am - 6am",
+      7=> "6am - 7am",
+      8=> "7am - 8am",
+      9=> "8am - 9am",
+      10=> "9am - 10am",
+      11=> "10am - 11am",
+      12=> "11am - noon",
+      13=> "noon - 1pm",
+      14=> "2pm - 3pm",
+      15=> "3pm - 4pm",
+      16=> "4pm - 5pm",
+      17=> "5pm - 6pm",
+      18=> "6pm - 7pm",
+      19=> "7pm - 8pm",
+      20=> "8pm - 9pm",
+      21=> "9pm - 10pm",
+      22=> "10pm - 11pm",
+      23=> "11pm - midnight" }
     event_hash = Hash.new
     event_name = []
     time_stamp = []
@@ -33,7 +53,7 @@ class Source < ActiveRecord::Base
     time_stamp.each do |i|
       hour << DateTime.parse(i).hour
     end
-    event_hash = event_name.zip(hour).map {|k,v| {k=>v}}
+    event_hash = event_name.zip(hour).map {|k,v| {k=>time_match_hash.fetch(v)}}
     event_hash.uniq
   end
 
