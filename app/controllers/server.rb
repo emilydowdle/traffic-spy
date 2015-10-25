50ae0ac085a96074bca8ced1883e651072d106c0
Marlo Major <MarloMajor@yahoo.com>
2015-10-25 01:39:15 -0600

    Some url logic

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -47,21 +47,25 @@ module TrafficSpy
     end
 
     get '/sources/:identifier/urls/:relative_path' do |identifier, relative_path|
-      @identifier = identifier
-      @relative_path = relative_path
-      location   = Source.find_by(identifier: identifier)
+      @identifier     = identifier
+      @relative_path  = relative_path
+      location        = Source.find_by(identifier: identifier)
       urls            = location.payloads.pluck(:url)
-      @http_verb      = location.payloads.pluck(:requestType)
-      @response_time  = location.payloads.pluck(:respondedIn)
-      @request_type   = location.payloads.pluck(:requestType)
-      @referred_by    = location.payloads.pluck(:referredBy)
-      @user_agent     = location.payloads.pluck(:userAgent)
+      @http_verb      = Payload.all.pluck(:requestType)
+      @response_time  = Payload.all.pluck(:respondedIn)
+      @request_type   = Payload.all.pluck(:requestType)
+      @referred_by    = Payload.all.pluck(:referredBy)
+      @agent          = Payload.all.pluck(:userAgent)
+      @platform       = UserAgent.parse(@agent.join).platform
+
 
       if !urls.include?("#{location.rootUrl}/#{relative_path}")
         erb :error
         @error_message = "URL has not been requested"
       else
+
         erb :urls
+
       end
 
 
